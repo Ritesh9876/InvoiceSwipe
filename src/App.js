61170ae6f4c45a218fd0740d563491d0bf5ebc93
Riesh9876 <riteshmangdare@gmail.com>
2024-03-03 17:09:40 +0530

    routing setup ready

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Container from 'react-bootstrap/Container';
+import './common.css'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import InvoiceForm from './components/InvoiceForm';
 import store from './reducers/store';
 import { Provider } from 'react-redux';
+import DisplayInvoices from './components/displayInvoices';
+import EditInvoice from './components/editInvoice';
 
 class App extends Component {
   render() {
   return (
-    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
+    <div 
+   // className="App d-flex flex-column align-items-center justify-content-center w-100"
+    >
       <Provider store={store}>
 
-      <Container>
-        <InvoiceForm/>
-      </Container>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<DisplayInvoices/>}/>
+          <Route exact path="/view" element={<>hello invoice</>}/>
+          <Route exact path="/add" element={<InvoiceForm/>}/>
+          <Route exact path="/edit" element = {<EditInvoice/>}/>
+        </Routes>
+      </Router>
       </Provider>
     </div>
   );
